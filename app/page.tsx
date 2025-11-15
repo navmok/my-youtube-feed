f70fa4431351f1ba13f0343c56715ea7c9c89dc0
navmok <navmok@users.noreply.github.com>
2025-11-15 04:12:22 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
               style={{
+                display: "block",
                 border: "1px solid #ddd",
                 borderRadius: "8px",
                 padding: "0.5rem",
